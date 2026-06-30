35832d23828855e531d7be85755735062a462236
bashar35790 <bashar35790@gamil.com>
2026-06-30 11:46:30 +0600

    redesign the navbar

--- index.php
+++ index.php
@@ -10,6 +10,30 @@
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <header id="header">
+        <div class="top_header">
+            <div class="container">
+                <p>Learning WordPress is fun! 😂</p>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand" href="#">Bashar2026</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link" href="#">Features</a>
+                        <a class="nav-link" href="#">Pricing</a>
+                        <a class="nav-link" href="#">Contact</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
     <h1>Hello Wordpress</h1>
 <?php wp_footer(); ?>
 </body>
