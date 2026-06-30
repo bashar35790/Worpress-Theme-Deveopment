7ebba0db9905ccc52dfe0ec86f792b4ebbea3b6d
bashar35790 <bashar35790@gamil.com>
2026-06-30 10:39:11 +0600

    added style function in the theme

--- functions.php
+++ functions.php
@@ -2,3 +2,9 @@
 
 add_theme_support('title-tag');
 add_theme_support('post-thumbnails');
+
+function bashar2026_enqueue_styles() {
+    wp_enqueue_style('bashar2026-style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'bashar2026_enqueue_styles');
