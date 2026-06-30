471f6b91b0e13e65acc438534a7b2de46013dd22
bashar35790 <bashar35790@gamil.com>
2026-06-30 10:57:51 +0600

    added bootstrap

--- functions.php
+++ functions.php
@@ -5,6 +5,10 @@ add_theme_support('post-thumbnails');
 
 function bashar2026_enqueue_styles() {
     wp_enqueue_style('bashar2026-style', get_stylesheet_uri());
+    wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '5.3.8', 'all');
+    wp_enqueue_style('custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all');
+    wp_enqueue_style("bootstrap");
+    wp_enqueue_style("custom");
 }
 
 add_action('wp_enqueue_scripts', 'bashar2026_enqueue_styles');
