ef84957278a62013bd7ad521f8d86bfe4a65e2f2
bashar35790 <bashar35790@gamil.com>
2026-06-30 11:40:14 +0600

    added navbar

--- functions.php
+++ functions.php
@@ -7,8 +7,11 @@ function bashar2026_enqueue_styles() {
     wp_enqueue_style('bashar2026-style', get_stylesheet_uri());
     wp_enqueue_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), '5.3.8', 'all');
     wp_enqueue_style('custom', get_template_directory_uri() . '/css/custom.css', array(), '1.0.0', 'all');
+    
     wp_enqueue_style("bootstrap");
     wp_enqueue_style("custom");
+
+    wp_enqueue_script('bootstrap-js', get_template_directory_uri() . '/js/bootstrap.min.js', array(), '5.3.8', true);
 }
 
 add_action('wp_enqueue_scripts', 'bashar2026_enqueue_styles');
